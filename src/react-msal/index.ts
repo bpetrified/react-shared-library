08691aed35a8c156e9bd4d5575a0a5187d37f0d1
chin <>
2023-02-12 12:00:24 +0700

    more export

--- src/react-msal/index.ts
+++ src/react-msal/index.ts
@@ -3,7 +3,7 @@ export * from '@azure/msal-react';
 export * from '@azure/msal-browser';
 export { version } from '@azure/msal-browser';
 export type { MsalLoginRequest } from './react-msal';
-export { useUserInGroups } from './hooks/user-in-groups';
+export { useUserInGroups, isIncludeInGroup } from './hooks/user-in-groups';
 export { useDataAfterAuthentication } from './hooks/data-after-authentication';
 export { WaitForAuthentication } from './components/wait-for-authentication';
 export { WaitForAuthenticationHelper } from './helpers/wait-for-authentication-helper';
