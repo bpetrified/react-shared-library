343fdecc0b197d3930707e918b8ef57b42fbe4e5
chin <>
2023-02-12 23:18:31 +0700

    fix type

--- src/react-msal/hooks/data-after-authentication.tsx
+++ src/react-msal/hooks/data-after-authentication.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { WaitForAuthenticationContext } from '../contexts/wait-for-authentication-context';
 
 export function useDataAfterAuthentication<T>() {
-  const context = React.useContext<T>(WaitForAuthenticationContext as any);
+  const context = React.useContext<{ data?: T }>(WaitForAuthenticationContext as any);
   if (context === undefined) {
     throw new Error('useDataAfterAuthentication must be used within a WaitForAuthenticationProvider');
   }
